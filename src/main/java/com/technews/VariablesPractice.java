2254665897533eae7675a7b7991f5d6a727d0c8f
sd8295642 <sd8295642@gmail.com>
2025-03-27 15:40:49 -0500

    added three demo classes to practice with Java, HelloWorld, VariablesPractice and VariableTypes

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.13;
+        String c = "hello";
+
+        double sum = a + b;
+        String concat = a + b + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
